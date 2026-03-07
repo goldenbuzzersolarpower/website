e89d186304bc61c10a805d96bef0af5afd6d3c61
ccadevida03 <c.cadevida03@gmail.com>
2026-03-07 21:01:32 +0800

    revise message

--- website/services/send-inquiry.php
+++ website/services/send-inquiry.php
@@ -7,24 +7,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $service = $_POST['service'];
     $phone = $_POST['phone'];
     $message = $_POST['message'];
-
+    $ticket = "GBSP" . date("Ymd") . "-" . rand(1000, 9999);
     $to = "goldenbuzzer.solarpowerph@gmail.com";
-    $subject = "New Solar Inquiry";
+    $subject = "Customer Inquiry - $ticket";
 
     $body = "
-New Inquiry Details
+Good day,
+
+You have received a new service inquiry from your website.
+
+--------------------------------------------------
+INQUIRY DETAILS
+--------------------------------------------------
 
 Name: $name
-Email: $email
-Phone: $phone
-Service: $service
+Email Address: $email
+Phone / Mobile Number: $phone
+Service Interested In: $service
+Ticket Number: $ticket
 
 Message:
 $message
+
+--------------------------------------------------
+
+Please respond to this inquiry at your earliest convenience.
+
+This message was automatically generated from your website inquiry form.
+
+Thank you.
+
+Best regards,
+Website Inquiry System
 ";
     $headers = "From: customerservice@goldenbuzzer-solarpower.com.ph\r\n";
     $headers .= "Reply-To: $email\r\n";
-    $headers .= "Cc: $email\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
     if (mail($to, $subject, $body, $headers)) {
